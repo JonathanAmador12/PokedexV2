928e39dc27f81d5ddc75c9e378edb7521457346d
Jonathan A <jamadorh91@gmail.com>
2022-10-08 20:26:16 -0500

    Initial Commit

---
+++ PokedexV2/PokedexV2App.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexV2App.swift
+//  PokedexV2
+//
+//  Created by JonathanA on 08/10/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
