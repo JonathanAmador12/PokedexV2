9aea557ca96e34cd1f6adfca4ec513f562fd4f67
Jonathan A <jamadorh91@gmail.com>
2022-10-17 12:05:42 -0500

    feature: create: -infinite scroll was created

--- PokedexV2/VIewModel/PokemonViewModel.swift
+++ PokedexV2/VIewModel/PokemonViewModel.swift
@@ -10,19 +10,30 @@ import Foundation
 
 class PokemonViewModel: ObservableObject {
     @Published var pokemon: [PokemonModel] = []
+    var limit: Int = 21
+    var offset: Int = 0
     
-    func getPokemos() {
+    func getPokemon() {
         let httpRequestHandler = HTTPRequestHandler()
-        let pokemonPageRequest = PokemonPageRequest()
+        let pokemonPageRequest = PokemonPageRequest(limit: limit, offset: offset)
         httpRequestHandler.requestData(requestData: pokemonPageRequest) {[weak self] result in
             switch (result){
             case .success(let pokemon):
                 DispatchQueue.main.async {
-                    self?.pokemon = pokemon
+                    for pokemon in pokemon {
+                        self?.pokemon.append(pokemon)
+                    }
                 }
             case .failure(let error):
                 print("\(error)")
             }
+        }
+    }
+    
+    func loadMoreData(name: String) {
+        if pokemon.last?.name == name {
+            offset = limit + offset
+            getPokemon()
         }
     }
 }
